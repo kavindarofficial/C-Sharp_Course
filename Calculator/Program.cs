76b80f79de915cdf755a3deb79b431dec9810bda
Kavindar <kavindarofficially@gmail.com>
2025-12-13 22:56:52 +0530

    Simple Calculator
    Assignment 1

---
+++ Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Input the first number:");
+string input = Console.ReadLine();
+int a = int.Parse(input);
+
+Console.WriteLine("Input the second number:");
+input = Console.ReadLine();
+int b = int.Parse(input);
+
+Console.WriteLine("What do you want to do with those numbers?\r\n[A]dd\r\n[S]ubtract\r\n[M]ultiply");
+input = Console.ReadLine();
+
+if (input == "A")
+{
+    Console.WriteLine(a + " + " + b + " = " + (a + b));
+}
+else if (input == "S")
+{
+    Console.WriteLine(a + " - " + b + " = " + (a - b));
+}
+else if (input == "M")
+{
+    Console.WriteLine(a + " * " + b + " = " + (a * b));
+}
+else
+{
+    Console.WriteLine("invalid option");
+}
+
+Console.ReadKey();
