3951373ea78ce9db4bf400d40adbb86a4d7dce35
Kavindar Arun <kavindar.arun@solitontech.com>
2026-01-09 18:00:21 +0530

    TriangleClass

---
+++ Triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Triangle
+{
+    //your code goes here
+    int Base, Height;
+
+    public Triangle(int @base,int @height)
+    {
+        Base = @base; Height = @height;
+    }
+
+    public int CalculateArea()
+    {
+        return (Base * Height)/2;
+    }
+
+    public string AsString()
+    {
+        return $"Base is {Base}, height is {Height}";
+    }
+}
